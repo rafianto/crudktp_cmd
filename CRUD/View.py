cfd59878295b5ffc25ff4f861c637f7a571627fa
rafianto <mulyonorafianto@gmail.com>
2022-11-19 10:19:33 +0700

    20221119-10.19

--- CRUD/View.py
+++ CRUD/View.py
@@ -4,21 +4,26 @@ def read_console():
     data_file = Operasi.read()
     
     index = "No"
-    judul = "NIK"
+    nik = "NIK"
     nama = "Nama"
+    alamat = "Alamat"
    
     # Header
     print("\n"+"="*100)
-    print(f"{index:4} | {nik:40} | {nama:40}")
+    print(f"{index:4} | {nik:16} | {nama:40} | {alamat:40}")
     print("-"*100)
     
     # Data
     for index,data in enumerate(data_file):
         data_break = data.split(",")
+        
         pk = data_break[0]
-        nik = data_break[2]
-        nama = data_break[3]
-        print(f"{index+1:4} | {nik:.40} | {nama:.40}",end="")
+        nik = data_break[1]
+        nama = data_break[2]
+        alamat = data_break[3]
+        
+        print(f"{index+1:4} | {nik:16} | {nama:.40} | {alamat:.40} ",end="\n")
+        
 
     # Footer
     print("="*100+"\n")
